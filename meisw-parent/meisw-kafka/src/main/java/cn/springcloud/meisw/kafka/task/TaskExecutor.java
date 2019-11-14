9ac883c987b3c71329372a8a2b4eacbd061117c8
qc_84f27166eb844408894ef14a8df4e3f0 <qc_84f27166eb844408894ef14a8df4e3f0@git.code.tencent.com>
2019-11-14 13:53:49 +0800

    test meisw-oracle-jpa多数据源生成的临时文件删除操作

---
+++ meisw-parent/meisw-kafka/src/main/java/cn/springcloud/meisw/kafka/task/TaskExecutor.java
@@ -0,0 +1,21 @@
+package cn.springcloud.meisw.kafka.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 任务调度器
+ * <p></p>
+ * @author meisw 2019年11月14日 上午10:36:40
+ * @ClassName TaskExecutor
+ * @Description TODO
+ * @version V1.0   
+ * @modificationHistory=========================逻辑或功能性重大变更记录
+ * @modify by user: {修改人} 2019年11月14日
+ * @modify by reason:{方法名}:{原因}
+ */
+public class TaskExecutor {
+	
+	private static final Logger log = LoggerFactory.getLogger(TaskExecutor.class);
+	
+}
