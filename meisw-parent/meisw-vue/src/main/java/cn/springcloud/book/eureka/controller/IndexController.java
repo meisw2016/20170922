dd1b99b13410e7209adbcf822eb6a96a0c4bcf91
meisw2016 <meisw_2016@163.com>
2019-11-21 00:15:20 +0800

    添加springboot-vue

---
+++ meisw-parent/meisw-vue/src/main/java/cn/springcloud/book/eureka/controller/IndexController.java
@@ -0,0 +1,13 @@
+//package cn.springcloud.book.eureka.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "/index")
+//public class IndexController {
+//	
+//	public String index(){
+//		return "index";
+//	}
+//}
