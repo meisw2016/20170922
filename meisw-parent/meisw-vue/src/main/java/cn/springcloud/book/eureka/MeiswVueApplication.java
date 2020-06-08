383cbe4c05e30ca3a8a4f96ce0363900eed0ede9
meisw2016 <meisw_2016@163.com>
2020-06-08 22:52:06 +0800

    添加vue自动创建页面项目

--- meisw-parent/meisw-vue/src/main/java/cn/springcloud/book/eureka/MeiswVueApplication.java
+++ meisw-parent/meisw-vue/src/main/java/cn/springcloud/book/eureka/MeiswVueApplication.java
@@ -2,13 +2,11 @@ package cn.springcloud.book.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * eureka server
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MeiswVueApplication {
 
     public static void main(String[] args) {
